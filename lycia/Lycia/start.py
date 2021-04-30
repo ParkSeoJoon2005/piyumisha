8e79352d7e464ebb2554448236fee4c935acf4cf
RedAura <79897294+Red-Aura@users.noreply.github.com>
2021-04-30 15:45:10 +0530

    Update start.py

--- lycia/Lycia/start.py
+++ lycia/Lycia/start.py
@@ -1,4 +1,4 @@
-from bot import LYCIA
+from lycia import LYCIA
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
@@ -10,6 +10,6 @@ Hello, I am Lycia [リュキア], An Intelligent ChatBot. If You Are Feeling Lon
 @LYCIA.on_message(filters.command(["start"], prefixes = "/") & ~filters.edited)
 async def info(client, message):
     buttons = [
-                [InlineKeyboardButton("Lycia", switch_inline_query_current_chat="lycia ", InlineKeyboardButton("Github", url = "https://github.com/Red-Aura/Lyciachatbot")]
+                [InlineKeyboardButton("Lycia", switch_inline_query_current_chat="lycia "), InlineKeyboardButton("Github", url = "https://github.com/Red-Aura/Lyciachatbot")]
               ]
     await LYCIA.send_message(chat_id = message.chat.id, text = LYCIA_START, reply_markup = InlineKeyboardMarkup(buttons))
