49c8815bf5a60ae4b2865b78e690d4fccd3362c0
RedAura <79897294+Red-Aura@users.noreply.github.com>
2021-04-30 15:43:17 +0530

    Update __main__.py

--- lycia/__main__.py
+++ lycia/__main__.py
@@ -1,5 +1,5 @@
 from lycia import LYCIA
-from lycia.Lycia import start, lycia, lyciainline, callback
+from lycia.Lycia import start, lycia, lyciainline
 from pyrogram import filters
 
 if __name__ == "__main__":
