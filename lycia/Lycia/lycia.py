7a76d68d950bea3078a9937fdc24f3927a846932
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-05-07 22:20:40 +0530

    Update lycia.py

--- lycia/Lycia/lycia.py
+++ lycia/Lycia/lycia.py
@@ -205,7 +205,7 @@ async def redaura(client, message):
 
 @LYCIA.on_message(
     filters.regex("Niha|niha|NIHA")
-    & ~filters.bot
+    & ~filters.bot("Niha|niha|NIHA")
     & ~filters.via_bot
     & ~filters.forwarded
     & ~filters.reply
