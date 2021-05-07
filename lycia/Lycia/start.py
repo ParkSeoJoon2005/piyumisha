1bfbf768f66b4dab4797e77c73549f0768030712
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-05-07 16:09:54 +0530

    Update start.py

--- lycia/Lycia/start.py
+++ lycia/Lycia/start.py
@@ -10,6 +10,6 @@ Hello, I am Nίհα, an AI Powered ChatBot. If You Are Feeling Lonely, You can A
 @LYCIA.on_message(filters.command(["start"], prefixes = "/") & ~filters.edited)
 async def info(client, message):
     buttons = [
-                [InlineKeyboardButton("Lycia", switch_inline_query_current_chat="lycia "), InlineKeyboardButton("Github", url = "https://t.me/EywaSC")]
+                [InlineKeyboardButton("Lycia", switch_inline_query_current_chat="niha"), InlineKeyboardButton("Github", url = "https://t.me/EywaSC")]
               ]
     await LYCIA.send_message(chat_id = message.chat.id, text = LYCIA_START, reply_markup = InlineKeyboardMarkup(buttons))
