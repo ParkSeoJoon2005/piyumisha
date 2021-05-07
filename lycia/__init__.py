053c10251908082e244ccdea33f579b74e8c9213
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-05-07 15:52:00 +0530

    Update __init__.py

--- lycia/__init__.py
+++ lycia/__init__.py
@@ -1,8 +1,8 @@
 import os
 from pyrogram import Client
 
-API_ID = os.environ.get("3609223")
-API_HASH = os.environ.get("302bf5498005e091fd23c88d8e76740e")
-TOKEN = os.environ.get("1831246366:AAHTE7bcvOasq_fWuI8EFgGiJ4cz0JYl4ek")
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+TOKEN = os.environ.get("TOKEN", None)
 
 LYCIA = Client(':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
